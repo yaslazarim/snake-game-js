87fe2c7378c1cd6a1485309214240ccdd8266c18
Yasmim Lazarim <yasslazarim@gmail.com>
2025-03-28 15:19:34 -0300

    Create drawSnake function

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+const size = 30
+
+const snake = [
+    {x:200, y:200},
+    {x:230, y:200},
+]
+
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd"
+
+    snake.forEach((position, index) => {
+
+        if(index == snake.length -1){
+            ctx.fillStyle = "darkmagenta"
+        }
+        ctx.fillRect(position.x, position.y, size, size)
+    })
+}
+
+drawSnake();
+
+
+
